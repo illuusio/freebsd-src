94449b68318731bc8bb3da6c1ba03163fe195733
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2026-04-01 13:24:01 +0300

    virtual_oss: Add SPDX-License-Identifier tags
    Some BSD-2-Clause files under lib/virtual_oss were
    missing the SPDX-License-Identifier-tag.
    
    Sponsored by: The FreeBSD Foundation

--- lib/virtual_oss/bt/sbc_encode.h
+++ lib/virtual_oss/bt/sbc_encode.h
@@ -1,6 +1,8 @@
 /* $NetBSD$ */
 
 /*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
  * Copyright (c) 2015 Nathanial Sloss <nathanialsloss@yahoo.com.au>
  *
  *		This software is dedicated to the memory of -
