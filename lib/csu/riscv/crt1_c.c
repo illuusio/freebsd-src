5be5c697334e3af041db5bd4913c691b6063d692
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2026-04-01 10:49:01 +0300

    csu: Add SPDX-License-Identifier tags
    Some BSD-2-Clause files under lib/csu were
    missing the SPDX-License-Identifier-tag.
    
    Sponsored by: The FreeBSD Foundation

--- lib/csu/riscv/crt1_c.c
+++ lib/csu/riscv/crt1_c.c
@@ -1,5 +1,7 @@
 /* LINTLIBRARY */
 /*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
  * Copyright 1996-1998 John D. Polstra.
  * Copyright (c) 2015-2017 Ruslan Bukin <br@bsdpad.com>
  * All rights reserved.
