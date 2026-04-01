83ff6950946acddf55ef11829416666fb3bc2bb6
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2026-04-01 12:21:21 +0300

    libsys: Add SPDX-License-Identifier tags
    Some BSD-2-Clause files under lib/libsyst were
    missing the SPDX-License-Identifier-tag.
    
    Sponsored by: The FreeBSD Foundation

--- lib/libsys/x86/__vdso_gettc.c
+++ lib/libsys/x86/__vdso_gettc.c
@@ -1,4 +1,6 @@
 /*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
  * Copyright (c) 2012 Konstantin Belousov <kib@FreeBSD.org>
  * Copyright (c) 2016, 2017, 2019 The FreeBSD Foundation
  * All rights reserved.
