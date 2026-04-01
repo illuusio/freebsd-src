53f5e0b2356c77dfb95999ef4bc08414a1f61deb
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2026-04-01 11:54:15 +0300

    libifconfig: Add SPDX-License-Identifier tags
    Some BSD-3-Clause and BSD-2-Clause files under lib/libifconfig
    were missing the SPDX-License-Identifier-tag.
    
    Sponsored by: The FreeBSD Foundation

--- lib/libifconfig/libifconfig.c
+++ lib/libifconfig/libifconfig.c
@@ -1,4 +1,6 @@
 /*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
  * Copyright (c) 1983, 1993
  *  The Regents of the University of California.  All rights reserved.
  * Copyright (c) 2016-2017, Marie Helene Kvello-Aune.  All rights reserved.
--- lib/libifconfig/libifconfig_sfp.c
+++ lib/libifconfig/libifconfig_sfp.c
@@ -1,4 +1,6 @@
 /*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
  * Copyright (c) 2014, Alexander V. Chernikov
  * Copyright (c) 2020, Ryan Moeller <freqlabs@FreeBSD.org>
  *
