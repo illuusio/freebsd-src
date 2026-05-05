3420c4b2dcd01eb91b737c6df56f1ff42369db69
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2026-05-05 13:40:43 +0300

    libc: Add SPDX-License-Identifier tags
    Some BSD-2-Clause AND LicenseRef-scancode-sudo files under lib/libc
    were missing the SPDX-License-Identifier-tag.
    
    Sponsored by: The FreeBSD Foundation

--- lib/libc/stdlib/getopt_long.c
+++ lib/libc/stdlib/getopt_long.c
@@ -2,6 +2,8 @@
 /*	$NetBSD: getopt_long.c,v 1.15 2002/01/31 22:43:40 tv Exp $	*/
 
 /*
+ * SPDX-License-Identifier: BSD-2-Clause AND LicenseRef-scancode-sudo
+ *
  * Copyright (c) 2002 Todd C. Miller <Todd.Miller@courtesan.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
